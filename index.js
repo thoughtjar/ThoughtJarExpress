f7fa4c7cf8f685359f70727fdd0bdfe90c0fa9c5
Kaushik <kaushik.sai@gmail.com>
2018-06-04 08:44:15 -0700

    Skeleton routes

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/createUser', function (req, res) {
+
+});
+
+app.post('/loginUser', function (req, res) {
+
+});
+
+app.post('/createSurvey', function (req, res) {
+
+});
+
+app.get('/respond', function (req, res) {
+  //token passed through url
+  //access token with req.query.uTx
 });
 
 app.listen(5000, () => console.log('Listening on port 5000 ...'));
