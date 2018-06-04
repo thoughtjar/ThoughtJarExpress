dd90c730f72631f496ef1f46148ea035a5d5f69e
xeliot <daveho8888@gmail.com>
2018-06-04 11:49:30 -0700

    add docs

---
+++ docs.md
@@ -0,0 +1,12 @@
+/createSurvey
+
+{
+  id: [questiontype, args ...]
+}
+
+example:
+{
+  "1": ["shortanswer", "Do you have a pet?"],
+  "2": ["longanswer", "Describe your house."],
+  "3": ["multiplechoice", "What is your favorite type of chocolate?", ["Dark", "Milk", "Orange"]]
+}
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = 'mongodb://kaushik:kaushik123@ds235609.mlab.com:35609';
+const mongodb = require('mongodb');
+const MONGO_URL = 'mongodb://kaushik:kaushik123@ds235609.mlab.com:35609/thought-jar-test';
 
 const app = express();
+
+var db;
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
@@ -12,16 +14,14 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use connect method to connect to the Server
-MongoClient.connect(MONGO_URL, function(err, client) {
-  console.log("Connected correctly to server");
-  const dbName = 'thought-jar-test';
-  const db = client.db(dbName);
+mongodb.MongoClient.connect(MONGO_URL, function(err, client) {
 
-  // Insert a single document
-  db.collection('surveys').insertOne({a:1}, function(err, r) {
-    client.close();
-  });
+  if(err) {
+    throw err;
+  }
+
+  db = client.db('thought-jar-test');
+
 });
 
 app.get('/', (req, res) => {
@@ -38,7 +38,40 @@ app.post('/loginUser', function (req, res) {
 
 app.post('/createSurvey', function (req, res) {
 
-});
+    var surveyCollection = db.collection('surveys');
+
+    let surveyData = [
+    {
+      decade: '1970s',
+      artist: 'Debby Boone',
+      song: 'You Light Up My Life',
+      weeksAtOne: 10
+    },
+    {
+      decade: '1980s',
+      artist: 'Olivia Newton-John',
+      song: 'Physical',
+      weeksAtOne: 10
+    },
+    {
+      decade: '1990s',
+      artist: 'Mariah Carey',
+      song: 'One Sweet Day',
+      weeksAtOne: 16
+    }
+  ];
+
+    surveyCollection.insert(surveyData, function(err, result) {
+
+      if(err) {
+        throw err;
+      } else {
+        res.send("success");
+      }
+
+    });
+
+  });
 
 app.get('/respond', function (req, res) {
   //token passed through url
